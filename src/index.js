000f609092da1b68d4cf396b3aedf6fa1393d27f
Pham Long Huan <huanvip455@gmail.com>
2021-05-19 14:20:45 +0700

    scss and create Navbar component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from './components/test';
-
+import Navbar from './components/Navbar';
 const Index = () => {
     return <div>WELCOME TO REACT APP NEW2 !</div>;
 };
 ReactDOM.render(
     <div>
+        <Navbar></Navbar>
         <Index />
         <Test />
+
         <h3>hello world</h3>
     </div>
     , document.getElementById('app')
