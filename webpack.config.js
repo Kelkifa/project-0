eef48472d8e28a094e3ae60db7ceabfd16223441
Pham Long Huan <huanvip455@gmail.com>
2021-05-19 15:26:32 +0700

    add scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "style-loader"],
+            },
         ]
     }
 };
